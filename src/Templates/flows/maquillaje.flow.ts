d0c60cc607129b8de1099deb19cd890a13622df6
Lichalaid <133549459+Lichalaid@users.noreply.github.com>
2025-02-27 22:59:47 -0300

    Update maquillaje.flow.ts

--- src/Templates/flows/maquillaje.flow.ts
+++ src/Templates/flows/maquillaje.flow.ts
@@ -7,8 +7,8 @@ import { config } from "~/config";
 const aiServiceInstance = new AiServices(config.ApiKey);
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const DEFAULT_PROFESIONAL = "Alejandra Cortez";
-const DEFAULT_DURATION = 2; // 2 hora
+const DEFAULT_PROFESIONAL = "Lifting de Pestañas";
+const DEFAULT_DURATION = 1; // 1 hora
 const DEFAULT_SERVICIO = "Maquillaje"
 const promptBase = `Eres un asistente virtual diseñado para ayudar a los usuarios a agendar citas mediante una conversación. 
 Tu objetivo es únicamente ayudar al usuario a elegir un horario y una fecha para sacar turno. 
