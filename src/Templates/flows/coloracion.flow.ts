9fee565adb48ead3cac9842111a7eed685ff6916
Lichalaid <133549459+Lichalaid@users.noreply.github.com>
2025-03-03 14:13:01 -0300

    Update coloracion.flow.ts

--- src/Templates/flows/coloracion.flow.ts
+++ src/Templates/flows/coloracion.flow.ts
@@ -9,7 +9,7 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const DEFAULT_PROFESIONAL = "BARBARA ROJAS (MICROPIGMENTACION)";
 const DEFAULT_DURATION = 2; // 2 hora
-const DEFAULT_SERVICIO = "Miropigmentacion de Cejas"
+const DEFAULT_SERVICIO = "MICROPIGMENTACION DE CEJAS"
 const promptBase = `Eres un asistente virtual diseñado para ayudar a los usuarios a agendar citas mediante una conversación. 
 Tu objetivo es únicamente ayudar al usuario a elegir un horario y una fecha para sacar turno. 
 Te voy a dar la fecha solicitada por el usuario y la disponibilidad de la misma. 
