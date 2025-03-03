2cb27c3744edda4faaae260b19a35e6cb83be1d5
Lichalaid <133549459+Lichalaid@users.noreply.github.com>
2025-03-03 14:13:23 -0300

    Update corte.cabello.flow.ts

--- src/Templates/flows/corte.cabello.flow.ts
+++ src/Templates/flows/corte.cabello.flow.ts
@@ -9,7 +9,7 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const DEFAULT_PROFESIONAL = "BARBARA ROJAS (MICROPIGMENTACION)";
 const DEFAULT_DURATION = 2; // 2 horas
-const DEFAULT_SERVICIO = "Micropigmentacion de Labios"
+const DEFAULT_SERVICIO = "MICROPIGMENTACION DE LABIOS"
 const promptBase = `Eres un asistente virtual diseñado para ayudar a los usuarios a agendar citas mediante una conversación. 
 Tu objetivo es únicamente ayudar al usuario a elegir un horario y una fecha para sacar turno. 
 Te voy a dar la fecha solicitada por el usuario y la disponibilidad de la misma. 
