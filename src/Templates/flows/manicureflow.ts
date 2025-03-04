e7b62da2796227679cbf35c62d7a2c7825abf191
Lichalaid <133549459+Lichalaid@users.noreply.github.com>
2025-03-04 16:42:36 -0300

    Update manicureflow.ts

--- src/Templates/flows/manicureflow.ts
+++ src/Templates/flows/manicureflow.ts
@@ -7,9 +7,9 @@ import { config } from "~/config";
 const aiServiceInstance = new AiServices(config.ApiKey);
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const DEFAULT_PROFESIONAL = "Barbara";
-const DEFAULT_DURATION = 1; // 1 hora
-const DEFAULT_SERVICIO = "LIFTING Y LAMINADO"
+const DEFAULT_PROFESIONAL = "BARBARA";
+const DEFAULT_DURATION = 2; // 2 hora
+const DEFAULT_SERVICIO = "EXTENSIONES Y REPOSICIÓN DE PESTAÑAS"
 const promptBase = `Eres un asistente virtual diseñado para ayudar a los usuarios a agendar citas mediante una conversación. 
 Tu objetivo es únicamente ayudar al usuario a elegir un horario y una fecha para sacar turno. 
 Te voy a dar la fecha solicitada por el usuario y la disponibilidad de la misma. 
