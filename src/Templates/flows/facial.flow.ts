386e2b984067274cf8b3f83cc9860a3d94f75bca
Lichalaid <133549459+Lichalaid@users.noreply.github.com>
2025-03-04 16:27:36 -0300

    Update facial.flow.ts

--- src/Templates/flows/facial.flow.ts
+++ src/Templates/flows/facial.flow.ts
@@ -7,9 +7,9 @@ import { config } from "~/config";
 const aiServiceInstance = new AiServices(config.ApiKey);
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const DEFAULT_PROFESIONAL = "Rudimar";
+const DEFAULT_PROFESIONAL = "RUDIMAR";
 const DEFAULT_DURATION = 2; // 2 hora
-const DEFAULT_SERVICIO = "Extensiones de Pestañas"
+const DEFAULT_SERVICIO = "MICROPIGMENTACION DE PARPADOS"
 const promptBase = `Eres un asistente virtual diseñado para ayudar a los usuarios a agendar citas mediante una conversación. 
 Tu objetivo es únicamente ayudar al usuario a elegir un horario y una fecha para sacar turno. 
 Te voy a dar la fecha solicitada por el usuario y la disponibilidad de la misma. 
